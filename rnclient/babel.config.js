0182c538b97604e82978fb720fff2b8f6e92f95d
Cundus <sundusardian@gmail.com>
2023-11-08 01:15:05 +0700

    rn init tailwind and aliases

--- rnclient/babel.config.js
+++ rnclient/babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'nativewind/babel',
+    [
+      'module-resolver',
+      {
+        alias: {
+          // This needs to be mirrored in tsconfig.json
+          // components: './src/components',
+          '@src': './src',
+        },
+      },
+    ],
+  ],
 };
---
+++ rnclient/src/styles/appClassnames.ts
@@ -0,0 +1,6 @@
+const APP_CLASSNAMES = {
+  CONTAINER: 'bg-red-200 flex-1 ',
+  FIRST_SECTION: 'flex-[3] bg-green-200',
+};
+
+export {APP_CLASSNAMES};
